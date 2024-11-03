f73931a4beb7941ea9dbb2af0b7650b4ea51781e
yimingsun12138 <yiming.sun12138@gmail.com>
2024-11-03 10:06:54 +0000

    add root user check upon module import

--- src/ColabGeek/__init__.py
+++ src/ColabGeek/__init__.py
@@ -1,1 +1,20 @@
-from .ColabGeek import *
+import os
+import warnings
+
+###########################
+## define setup function ##
+###########################
+
+# root user check
+def _root_user_check():
+    """Check if the current user is root."""
+
+    if (os.geteuid() != 0):
+        warnings.warn(message = "The current user is not root, some features may not work properly!",category = UserWarning)
+
+###############################
+## action upon module import ##
+###############################
+
+# root user check
+_root_user_check()
