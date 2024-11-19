6acbaf4e0ee270875e3f03691ba81c56b8d9c515
yimingsun12138 <yiming.sun12138@gmail.com>
2024-11-19 16:14:49 +0000

    check previous code optimization

--- src/ColabGeek/shell_scripts/Run_Rstudio_server.sh
+++ src/ColabGeek/shell_scripts/Run_Rstudio_server.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
 # get parameter
-installation_path=$1
+tmp_path=$1
 Rstudio_server_port=$2
 
 # go to temp installation path
-cd /tmp/$installation_path
+cd /tmp/$tmp_path
 
 # install Rstudio server
-wget https://s3.ap-east-1.amazonaws.com/mrdoge-s3-bucket/share/resource_for_download/rstudio_server_for_Google_Colab.deb -O /tmp/$installation_path/rstudio_server.deb
-gdebi -n /tmp/$installation_path/rstudio_server.deb
+wget https://s3.ap-east-1.amazonaws.com/mrdoge-s3-bucket/share/resource_for_download/rstudio_server_for_Google_Colab.deb -O /tmp/$tmp_path/rstudio_server.deb
+gdebi -n /tmp/$tmp_path/rstudio_server.deb
 
 # configure Rstudio server port
 echo "www-port=$Rstudio_server_port" > /etc/rstudio/rserver.conf
